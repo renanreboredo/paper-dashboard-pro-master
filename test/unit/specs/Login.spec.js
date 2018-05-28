f2c7ef63b76a24fb7d5464cd7695c0ca54a65b3f
Renan Reboredo <renanrreboredo@gmail.com>
2018-05-28 12:20:43 -0300

    Remove karma test and insert first jest test

---
+++ test/unit/specs/Login.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils'
+import Login from '@/components/Dashboard/Views/Pages/Login.vue'
+
+describe('Login.vue', () => {
+  it('works', () => {
+    expect(true).toEqual(true)
+  })
+})
